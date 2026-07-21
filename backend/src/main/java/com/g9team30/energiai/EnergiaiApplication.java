2d2aba184564d32d6253b093ae347e209584744e
Mabeliris <mabellecfernandez@gmail.com>
2026-07-20 20:13:57 -0400

    Feat:initial backend directory setup with Maven and .gitattributes

---
+++ backend/src/main/java/com/g9team30/energiai/EnergiaiApplication.java
@@ -0,0 +1,13 @@
+package com.g9team30.energiai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnergiaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnergiaiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/g9team30/energiai/EnergiaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.g9team30.energiai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnergiaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
