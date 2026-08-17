d5af2a230b57aab3a705258373e4279932d11860
skwrs <sskewer7@gmail.com>
2026-08-16 21:40:05 -0400

    feat: implementar AnalisisService para gestión de análisis energéticos

--- backend/src/main/java/com/g9team30/energiai/infra/persistence/entity/AnalisisEnergetico.java
+++ backend/src/main/java/com/g9team30/energiai/infra/persistence/entity/AnalisisEnergetico.java
@@ -3,6 +3,7 @@ package com.g9team30.energiai.infra.persistence.entity;
 import com.g9team30.energiai.domain.common.enums.Categoria;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -13,6 +14,7 @@ import java.time.LocalDateTime;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class AnalisisEnergetico {
 
     @Id
